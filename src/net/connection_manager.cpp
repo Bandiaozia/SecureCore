224c92d28f9b8b7839df24e2586d8ef95942ea03
ban <2931325659@qq.com>
2026-07-31 20:40:18 +0800

    feat: enforce HTTP connection limit

--- src/net/connection_manager.cpp
+++ src/net/connection_manager.cpp
@@ -1,16 +1,35 @@
 #include "secure/net/connection_manager.hpp"
 
+#include <stdexcept>
 #include <vector>
 
 namespace secure {
 
-void ConnectionManager::start(
+ConnectionManager::ConnectionManager(
+    std::size_t max_connections
+)
+    : max_connections_(max_connections) {
+    if (max_connections_ == 0) {
+        throw std::invalid_argument(
+            "max_connections must be greater than zero"
+        );
+    }
+}
+
+bool ConnectionManager::start(
     const std::shared_ptr<Connection>& connection
 ) {
     bool inserted = false;
 
     {
         std::scoped_lock lock(mutex_);
+
+        if (
+            connections_.size() >=
+            max_connections_
+        ) {
+            return false;
+        }
 
         inserted =
             connections_.insert(connection).second;
@@ -19,6 +38,8 @@ void ConnectionManager::start(
     if (inserted) {
         connection->start();
     }
+
+    return inserted;
 }
 
 void ConnectionManager::remove(
@@ -53,6 +74,20 @@ std::size_t ConnectionManager::size() const {
     std::scoped_lock lock(mutex_);
 
     return connections_.size();
+}
+
+std::size_t
+ConnectionManager::capacity() const noexcept {
+    return max_connections_;
+}
+
+bool ConnectionManager::full() const {
+    std::scoped_lock lock(mutex_);
+
+    return (
+        connections_.size() >=
+        max_connections_
+    );
 }
 
 }  // namespace secure
